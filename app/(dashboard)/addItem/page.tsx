d99cb163684392fc8d898906aa3dd8c6faf6e774
solopaw <adamjoelfraser@icloud.com>
2024-10-06 15:04:58 +1000

    modified:   app/(dashboard)/addItem/page.tsx

--- app/(dashboard)/addItem/page.tsx
+++ app/(dashboard)/addItem/page.tsx
@@ -17,33 +17,3 @@ export default async function AddItemPage() {
   );
 }
 
-async function onAddButtonClick() {
-  const { totalProducts } = await getProducts('', 0);
-  db.insert(products).values([
-    {
-      id: totalProducts + 1,
-      status: 'active',
-      availableAt: new Date(),
-
-      name,
-      imageUrl: image,
-      price,
-      stock
-    }
-  ]);
-  const router = useRouter();
-  router.push('/');
-}
-function onPriceChanged(e: FormEvent<HTMLInputElement>) {
-  price = e.currentTarget.value;
-}
-function onImageChanged(e: FormEvent<HTMLInputElement>) {
-  image = e.currentTarget.value;
-}
-
-function onNameChanged(e: FormEvent<HTMLInputElement>) {
-  name = e.currentTarget.value;
-}
-function onStockChanged(e: FormEvent<HTMLInputElement>): void {
-  stock = e.currentTarget.valueAsNumber;
-}
